c5f70a7d1129c816f4524b83f881c1c7d7b02ddf
seethamchokkapu <seethamnaidu.chokkapu@feuji.com>
2022-11-21 10:42:00 +0530

    new

--- atm-task-14.js
+++ atm-task-14.js
@@ -1,11 +1,15 @@
-  var accno=["939233","63050"];
-  var pswd=["1234","5678"];
-  var bln=[670,1769];
+  var accno=["939233","63050","957325","996655"];
+  var pswd=["1234","5678","9012","3456"];
+  var bln=[670,1769,5400,3200];
   var currentUser=0;
   var DepositMoney;
-  var transactions=[[],[]];
+  var transactions=[[],[],[],[]];
   var tble=document.getElementById("tableId");
-
+  var DepoHistory=document.getElementById("depo-history");
+  var WithHistory=document.getElementById("with-history");
+   var transButton=document.getElementById("btn-t");
+   var tabledata=document.getElementsByTagName("tr");
+  var h=0;var j=0;
 document.getElementById("modal").onclick=function(){
   
   document.querySelector(".div1").style.display="block";
@@ -20,18 +24,22 @@ document.getElementById("modal").onclick=function(){
        var upswd=document.getElementById("userpassword-validation").value;
         
        if(accno.indexOf(uAcno)==-1){
-       
         alert("enter valid account number");
-       
-       }else{
+         if(uAcno==""){
+       document.querySelector(".expand").style.display="block";
+     //  document.querySelector(".div1").style.display="none";
+       }
+       }
+      else
+         {
        
           if(upswd==pswd[accno.indexOf(uAcno)]){
-          // alert(" correct password ");
+          
            currentUser=accno.indexOf(uAcno);
             document.querySelector(".options-page").style.display="block";
             document.querySelector(".div1").style.display="none";
            
-            //alert( bln[currentUser]);
+           
             
             document.getElementById("withdraw").innerHTML=bln[currentUser];
              document.getElementById("placed").innerHTML=uAcno;
@@ -39,13 +47,17 @@ document.getElementById("modal").onclick=function(){
              document.getElementById("userpassword-validation").value="";
             
           
-          }else{
-             alert("invalid password")
+          }
+          else
+          {
+             alert("invalid password");
+            
           }
        
        }
-     
+       
    }
+   
   function cancle(){
     document.querySelector(".options-page").style.display="block";
     document.querySelector(".page-none").style.display="none";
@@ -120,35 +132,78 @@ document.getElementById("modal").onclick=function(){
             
            
            }
-   //function transactions  >>>>>>>>>>>>>>>>>     
+           transButton.onclick=function(){
+         tranHistory("all");
+     }
            
-           document.getElementById("btn-t").onclick=function(){
+    DepoHistory.onclick=function(){
+    close();
+        tranHistory("d");
+     
+       document.querySelector(".clear").style.display="none";
+       
+    }        
+   WithHistory.onclick=function(){
+   close();
+        tranHistory("w");
+        document.querySelector(".clear").style.display="none";
+      
+      
+    }        
+   //function transactions  >>>>>>>>>>>>>>>>>  
+      function tranHistory(type)  {   
+        alert(type);
+          // document.getElementById("btn-t").onclick=function(){
            document.querySelector(".options-page").style.display="block";
             document.querySelector(".last-page").style.display="block";
            document.querySelector(".options-page").style.display="none";
-          for(var j=0;j<transactions[currentUser].length;j++){
+          for( j=0;j<transactions[currentUser].length;j++){
           
-             var tabledata=document.getElementsByTagName("tr");
+           // var tabledata=document.getElementsByTagName("tr");
              tble.innerHTML+=`<tr>
              
              <td>${transactions[currentUser][j].type}</td>
              <td>${transactions[currentUser][j].amount}</td>
              <td>${transactions[currentUser][j].balance}</td>
              </tr>`
+             if(type=="all"){
              if(transactions[currentUser][j].type=="withdraw"){
              tabledata[j+1].style.color="red";
              }
-             else 
+             else
              {
                    tabledata[j+1].style.color="green";
              }
+             }
+             else if(type=="d"){
+            
+                 if(transactions[currentUser][j].type=="withdraw")
+                 {
+             tabledata[j+1].style.display="none";
+            
+            
+                 
+             }else{
+                 tabledata[j+1].style.color="green";
+             }
+          }else{
+         
+              if(transactions[currentUser][j].type=="withdraw")
+              {
+                  tabledata[j+1].style.color="red";
+                  
+              }else{
+                  tabledata[j+1].style.display="none";
+              }
           }
            
            
-           
+          }
            }
-   //function close >>>>>>>>>>>>>>>>>>>>>>>>>>>        
-    document.getElementById("bclose").onclick=function(){
+         
+   //function close >>>>>>>>>>>>>>>>>>>>>>>>>>> 
+   function close(){       
+    //document.getElementById("bclose").onclick=function(){
      document.querySelector(".last-page").style.display="none";
       document.querySelector(".clear").style.display="block";
       document.getElementById("tableId");
@@ -158,15 +213,13 @@ document.getElementById("modal").onclick=function(){
             <th>Amount</th>
             <th>Total Balance</th>
           </tr>`
- 
- 
+   
  }          
- 
+ //function cancle button in second page  >>>>>>>>>>>>>>
  document.getElementById("Btn").onclick=function(){
   document.querySelector(".div1").style.display="none";
  document.querySelector(".clear").style.display="block";
  }
-     
 
-
+ 
 
