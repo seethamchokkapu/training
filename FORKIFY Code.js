4af7a48fead4bf08efad5e8fba004df4f51fd210
seethamchokkapu <116893886+seethamchokkapu@users.noreply.github.com>
2022-12-06 16:30:08 +0530

    Add files via upload

--- FORKIFY Code.js
+++ FORKIFY Code.js
@@ -9,41 +9,51 @@ var nextButton=document.querySelector(".btnpage5");
   nextButton.addEventListener("click",nextPage);
    previousButton.addEventListener("click",prevPage);
   
-  
-  
+ nextButton.innerHTML=`page ${page+1}<img src="images/right_arrowicon (1).svg" class="arrow">`;
+  previousButton.style.display="none";
    initialPage();
   
 function initialPage(){
-  for( var i=(page*10)-10;i<page*10;i++){
+                 for( var i=(page*10)-10;i<page*10;i++){
         
-	       sideContent.innerHTML+=`<li>
-		   <div class="imgbtn">
+	                  sideContent.innerHTML+=`<li>
+		              <div class="imgbtn">
 		   
-		   <img src="${jsonData.recipes[i].image_url}"class="imagepizza">
-		   <span class="spanclr">${jsonData.recipes[i].title}</span> 
+		                   <img src="${jsonData.recipes[i].image_url}"class="imagepizza">
+		                   <span class="spanclr">${jsonData.recipes[i].title}</span> 
 		   
-		   <p class="text-left">${jsonData.recipes[i].publisher}</p> 
-		   </div>
-		    </li>`       
-  }
-
-}
+		                   <p class="text-left">${jsonData.recipes[i].publisher}</p> 
+		             </div>
+		          </li>`       
+                              }
+      
+	 
+                              }
  
   
   
  function nextPage(){
-	  sideContent.innerHTML="";
-	  page++;
+	         sideContent.innerHTML="";
+	         page++;
 	   
 	   
-	 if(page*10>28)  {
+	         if(page*10>28)  {
 		 
-		   nextButton.style.display="none";
-		   previousButton.style.display="block";
-	 }		 
-	 initialPage();
- }
- 
+		     nextButton.style.display="none";
+		      previousButton.style.display="block";
+		   
+	         }	
+	        else{
+		
+		      previousButton.style.display="block";
+		 
+	              }
+	      
+	         nextButton.innerHTML=`page ${page+1}<img src="images/right_arrowicon (1).svg" class="arrow">`;
+		     previousButton.innerHTML=`<img src="images/left_icon (1).svg" class="arrow">page ${page-1}`;
+	         initialPage();
+                               }
+  
   function prevPage(){
 	
 	    sideContent.innerHTML="";
@@ -51,7 +61,14 @@ function initialPage(){
 		if(page*10==10){
 			previousButton.style.display="none";
 			nextButton.style.display="block";
+			
 		}
-     initialPage();
+		else
+		{
+			nextButton.style.display="block";
+		}
+		previousButton.innerHTML=`<img src="images/left_icon (1).svg" class="arrow">page ${page-1}`;
+		nextButton.innerHTML=`page ${page+1}<img src="images/right_arrowicon (1).svg" class="arrow">`;
+       initialPage();
   }
   
