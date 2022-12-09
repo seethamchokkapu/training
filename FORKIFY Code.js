46e4009cb122b8406e6720e2e16eaed2765ceec1
seethamchokkapu <seethamnaidu.chokkapu@feuji.com>
2022-12-09 17:57:31 +0530

    newcontent

--- FORKIFY Code.js
+++ FORKIFY Code.js
@@ -1,43 +1,59 @@
-var page=1;
+var pageNumber=1;
+var recipesList;
 var sideContent=document.querySelector(".side-content");
 var previousButton=document.querySelector(".btnpage3");
 var nextButton=document.querySelector(".btnpage5");
+var pageValue=10;
+var loopInitilizer=(pageNumber*pageValue)-pageValue;
+var valueEight;
 
 
+var request=new XMLHttpRequest();
+   console.log(request);
+   request.open("GET","https://forkify-api.herokuapp.com/api/search?q=pizza");
+   request.send();
+  
+  request.addEventListener("load",function(){
+	  recipesList=JSON.parse(this.responseText);
+      console.log(recipesList);
+	  valueEight = recipesList.count % pageValue;
+	    initialPage();
+   });
 
 
   nextButton.addEventListener("click",nextPage);
    previousButton.addEventListener("click",prevPage);
   
- nextButton.innerHTML=`page ${page+1}<img src="images/right_arrowicon (1).svg" class="arrow">`;
+ nextButton.innerHTML=`page ${pageNumber+1}<img src="images/right_arrowicon (1).svg" class="arrow">`;
   previousButton.style.display="none";
-   initialPage();
+  
   
 function initialPage(){
-                 for( var i=(page*10)-10;i<page*10;i++){
+	   
+                 for( var i=(pageNumber*pageValue)-pageValue;i<(pageNumber*pageValue);i++){
         
 	                  sideContent.innerHTML+=`<li>
 		              <div class="imgbtn">
 		   
-		                   <img src="${jsonData.recipes[i].image_url}"class="imagepizza">
-		                   <span class="spanclr">${jsonData.recipes[i].title}</span> 
+		                  <img  class="imagepizza" src="${recipesList.recipes[i].image_url}" >
+		                   <span class="spanclr">${recipesList.recipes[i].title}</span> 
 		   
-		                   <p class="text-left">${jsonData.recipes[i].publisher}</p> 
+		                   <p class="text-left">${recipesList.recipes[i].publisher}</p> 
 		             </div>
 		          </li>`       
                               }
-      
+	
 	 
                               }
- 
+
   
   
  function nextPage(){
-	         sideContent.innerHTML="";
-	         page++;
+	        
+	         pageNumber++;
+	      sideContent.innerHTML="";
 	   
-	   
-	         if(page*10>28)  {
+	         if(pageNumber*pageValue> recipesList.count)  {
 		 
 		     nextButton.style.display="none";
 		      previousButton.style.display="block";
@@ -49,16 +65,22 @@ function initialPage(){
 		 
 	              }
 	      
-	         nextButton.innerHTML=`page ${page+1}<img src="images/right_arrowicon (1).svg" class="arrow">`;
-		     previousButton.innerHTML=`<img src="images/left_icon (1).svg" class="arrow">page ${page-1}`;
+	         nextButton.innerHTML=`page ${pageNumber+1}<img src="images/right_arrowicon (1).svg" class="arrow">`;
+		     previousButton.innerHTML=`<img src="images/left_icon (1).svg" class="arrow">page ${pageNumber-1}`;
+			
+			if(pageNumber==3){
+				lastPageErr();
+			}else{
 	         initialPage();
+			}
+			
                                }
   
   function prevPage(){
 	
 	    sideContent.innerHTML="";
-		page--;
-		if(page*10==10){
+		pageNumber--;
+		if(pageNumber*pageValue==pageValue){
 			previousButton.style.display="none";
 			nextButton.style.display="block";
 			
@@ -67,8 +89,29 @@ function initialPage(){
 		{
 			nextButton.style.display="block";
 		}
-		previousButton.innerHTML=`<img src="images/left_icon (1).svg" class="arrow">page ${page-1}`;
-		nextButton.innerHTML=`page ${page+1}<img src="images/right_arrowicon (1).svg" class="arrow">`;
+		previousButton.innerHTML=`<img src="images/left_icon (1).svg" class="arrow">page ${pageNumber-1}`;
+		nextButton.innerHTML=`page ${pageNumber+1}<img src="images/right_arrowicon (1).svg" class="arrow">`;
        initialPage();
   }
+  function lastPageErr(){
+	  
+	
+		   
+		
+	     for( var i=(pageNumber*pageValue)-pageValue;i<((pageNumber*pageValue)-pageValue)+valueEight;i++){
+			  
+        
+	                  sideContent.innerHTML+=`<li>
+		              <div class="imgbtn">
+		   
+		                   <img src="${recipesList.recipes[i].image_url}"class="imagepizza">
+		                   <span class="spanclr">${recipesList.recipes[i].title}</span> 
+		   
+		                   <p class="text-left">${recipesList.recipes[i].publisher}</p> 
+		             </div>
+		          </li>`       
+                              }
+	   
+	   
+                              }
   
