626717624e0534005adccdaba361a120ae5dbab1
seethamchokkapu <seethamnaidu.chokkapu@feuji.com>
2023-01-27 12:06:59 +0530

    spaTask

--- TASK-SPA/JS/script.js
+++ TASK-SPA/JS/script.js
@@ -3,41 +3,47 @@ let about="";
 let contact="";
 let page404="";
 let contentDiv=document.getElementById("empty-div");
+ let routes;
+
 async function loadPage(page){
     let response=await fetch(page);
     let resHtml=await response.text();
     return resHtml;
-     
 }
+
 async function loadAllpages(){
     home=await loadPage("home.html");
     about=await loadPage("about.html");
     contact=await loadPage("contact.html");
     page404=await loadPage("404.html");
 }
+
 async function main(){
    await loadAllpages();
-    contentDiv.innerHTML=home; 
-   routes={
-    "/":home,
-    "/about":about,
-    "/contact":contact,
-    "**":page404
-   }
-  
+   contentDiv.innerHTML=home; 
+    routes={
+        "#home":home,
+        "#about":about,
+        "#contact":contact,
+        "**":page404
+       }    
 }
 main();
-
-async function anchorTagCall(pathname){
-   
-    
-        window.history.pushState({},pathname,location.origin+pathname);
-        contentDiv.innerHTML=routes[pathname];   
-    
-   
-      
+async function anchorTagCall(){
+        window.history.pushState({},location.hash,location.hash);       
+         
 }
-
+window.addEventListener("hashchange",anchorTagCall);
 window.onpopstate=()=>{
-    contentDiv.innerHTML=routes[window.location.pathname]
+    if(!routes[location.hash]){
+        console.log(location.hash)
+        contentDiv.innerHTML=routes["**"];
+    }
+    else{
+    console.log(routes[location.hash]);
+    contentDiv.innerHTML=routes[location.hash]
+    }
 }
+
+
+
